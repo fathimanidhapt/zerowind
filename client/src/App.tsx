f25604dc23c67873e93ae920690f14245e35f2f5
fathimanidha <fnidha788@gmail.com>
2026-08-03 15:49:24 +0530

    clean: remove comments in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,10 @@ import { BrowserRouter, useLocation } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import AppRoutes from "./routes/AppRoutes";
 
-// Scroll restoration helper
 function ScrollToTop() {
   const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    // If there is no hash anchor, scroll to top on path change
     if (!hash) {
       window.scrollTo(0, 0);
     }
